e8c1ff50153950fd92549698558e6d6722a791dd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-15 20:54:15 +0000

    automatically backed up by learn

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,9 @@
+function hideWhenClicked() {
+  let hide = document.getElementById("hide_this")
+  if (hide.style.display !== "none") {
+    hide.style.display === "none"
+  } else {
+    hide.style.display === "block"
+  }
+}
+
